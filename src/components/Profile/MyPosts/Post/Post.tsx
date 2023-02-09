d7a54be72e799c03408f57a75aafc3fccb8e062c
Dzmitry Murauyou <murauyou.dzmitry@gmail.com>
2023-02-09 19:12:18 +0300

    15 lesson end

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Post.module.css';
+
+function Post() {
+    return (
+        <div className={s.item}>
+            <img
+                src="https://st2.depositphotos.com/2872743/10119/i/600/depositphotos_101190820-stock-photo-cartoon-mustache-face-cute-emoticon.jpg"/>
+            post1
+            <div>
+                <span>like</span>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Post;
